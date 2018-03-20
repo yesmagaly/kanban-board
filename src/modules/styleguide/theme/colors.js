c0d0056cf2340cec7f7478d29dd08da8008c8895
Yesica Ramirez <yesmagaly@gmail.com>
2018-03-20 16:16:32 -0500

    Added lightBlue color and renamed gray to grey color.

--- src/modules/styleguide/theme/colors.js
+++ src/modules/styleguide/theme/colors.js
@@ -1,10 +1,11 @@
 export default {
   red: '#ff0000',
   blue: '#4680ff',
+  lightBlue: '#dce5fe',
   white: '#fff',
   pink: '#fb617B',
   yellow: '#feb64d',
-  gray: '#ebeef4',
+  grey: '#ebeef4',
   green: '#a9d267',
   purple: '#8b7fc8',
 }
